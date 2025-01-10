518d1e0935b9c64b6fc84d713d8c943b091b2e13
swebber-upngo <85571381+swebber-upngo@users.noreply.github.com>
2025-01-10 10:33:55 -0500

    Updated aerospace config to remember desired windows and restore on workspace change.
    TODO: layout is still a bit hacky. 1 is always accordion, others are
    tiles, and only horizontal. This is not stricly suitable for remembering
    any sort of complex layout

--- aerospace/.config/aerospace/auto-config.aerospace.sh
+++ aerospace/.config/aerospace/auto-config.aerospace.sh
@@ -2,8 +2,18 @@
 
 CWD=$(pwd)
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TOML_FILE="$SCRIPT_DIR/.workspace-window-modes.toml"
 
+CURRENT_MONITOR=$(aerospace list-monitors --focused --format %{monitor-id})
 XDR_ID=$(aerospace list-monitors | grep XDR | awk '{print $1}')
+
+if [[ "$CURRENT_MONITOR" -ne "$XDR_ID" ]]; then
+  echo Current monitor is not XDR. Exiting
+  exit 0
+fi
+
+CURRENT_WORKSPACE=$(aerospace list-workspaces --focused)
+SAVED_MODE=$(cat "$TOML_FILE" | grep "^$CURRENT_WORKSPACE = " | sed -e"s/^$CURRENT_WORKSPACE = //")
 MONITOR_COUNT=$(aerospace list-monitors --count)
 
 WINDOW_COUNT=3
@@ -14,25 +24,46 @@ fi
 if [ "$WINDOW_COUNT" -eq 0 ]; then
   WINDOW_COUNT=1
 fi
-
+echo WINDOW_COUNT: $WINDOW_COUNT
 WINDOW_CHOICE_DESIRED=${1:-$WINDOW_COUNT}
+
+if [ "$WINDOW_CHOICE_DESIRED" == "W" ]; then
+  # Use saved mode if it exists
+  if [ "$SAVED_MODE" != "" ]; then
+    echo Restoring mode $SAVED_MODE
+    WINDOW_CHOICE_DESIRED=$SAVED_MODE
+  else
+    echo Defaulting to $WINDOW_COUNT windows
+    WINDOW_CHOICE_DESIRED=$WINDOW_COUNT
+  fi
+fi
+
 GAP_ADJUSTMENT=$((30 / 2 * (WINDOW_COUNT - 1)))
 
 GAPS="n-windows.gaps.toml"
+LAYOUT=tiles
 
 if [ "$WINDOW_CHOICE_DESIRED" -eq 1 ]; then
-  echo 1
+  echo Desired mode 1
   GAPS="one-window.gaps.toml"
+  LAYOUT=accordion
 elif [ "$WINDOW_CHOICE_DESIRED" -eq 2 ]; then
-  echo 2
+  echo Desired mode 2
   GAPS="two-windows.gaps.toml"
+else
+  echo Desired mode 3
 fi
 
-# write config file, then reload it
 cd "$SCRIPT_DIR" || false
+# write config file, then reload it
 cat globals.toml >aerospace.toml
 # // offset gaps for tiled windows
 cat "$GAPS" | perl -pe"s/850/$((850 - $GAP_ADJUSTMENT))/" >>aerospace.toml
 cat modes.toml >>aerospace.toml
+aerospace layout $LAYOUT horizontal
 aerospace reload-config
+
+# save mode for later
+. "$SCRIPT_DIR/save-window-mode.sh"
+
 cd "$CWD" || false
---
+++ aerospace/.config/aerospace/save-window-mode.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TOML_FILE="$SCRIPT_DIR/.workspace-window-modes.toml"
+
+WORKSPACE=$(aerospace list-workspaces --focused)
+CURRENT_MODE=$(cat "${SCRIPT_DIR}/aerospace.toml" | grep WINDOW-MODE | sed -e's/^.*WINDOW-MODE-//')
+
+if grep -q "^${WORKSPACE} = " "$TOML_FILE"; then
+  echo "Editing $WORKSPACE = $CURRENT_MODE"
+  perl -i -pe"s/$WORKSPACE = .*/$WORKSPACE = $CURRENT_MODE/" "$TOML_FILE"
+else
+  echo "Adding $WORKSPACE = $CURRENT_MODE"
+  echo "$WORKSPACE = $CURRENT_MODE" >>"$TOML_FILE"
+fi
